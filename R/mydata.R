11ee9c7169ae24bb61f76bf9212b43f83f8f86ad
younthing <fanxingfu3344@gmail.com>
2024-09-19 14:13:06 +0800

    chore: build project

--- R/mydata.R
+++ R/mydata.R
@@ -1,7 +1,7 @@
 # Generated from create-rnaseq.Rmd: do not edit by hand
 
 #' A regression data set
-#' 
+#'
 #' @format A data frame with 100 rows and 2 variables:
 #' \describe{
 #' \item{x}{a predictor variable}
